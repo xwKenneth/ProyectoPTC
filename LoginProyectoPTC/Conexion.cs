b4abc309914ef6fbc945dbec3a8be55984a39a53
xwKenneth <101608878+xwKenneth@users.noreply.github.com>
2022-06-15 17:22:11 -0600

    Parte 1
    Solo subi la parte de login y el dashbord pero aun faltan las demas partes y arreglar el dashboard

---
+++ LoginProyectoPTC/Conexion.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+
+namespace SBPA
+{
+    internal class Conexion
+    {
+        /*
+        string usuario, servidor, db, cadena;
+        MySqlConnection con;
+        public Conexion()
+        {
+            usuario = "root";
+            servidor = "127.0.0.1";
+            db = "testEmpresa3c";
+            cadena = "server=" + servidor + ";uid=" +
+                usuario + ";" + "database=" + db;
+
+            con = new MySqlConnection(cadena);
+        }
+
+        public MySqlConnection Conectar()
+        {
+            return con;
+        }
+        */
+    }
+}
---
+++ LoginProyectoPTC/DashBoard.Designer.cs
@@ -0,0 +1,252 @@
+﻿
+namespace SBPA
+{
+    partial class dashboard
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.btnBuscar = new System.Windows.Forms.Button();
+            this.btnPrestamos = new System.Windows.Forms.Button();
+            this.btnCerrar = new System.Windows.Forms.Button();
+            this.btnCertificados = new System.Windows.Forms.Button();
+            this.btnHipotecarios = new System.Windows.Forms.Button();
+            this.btnAhorro = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pnlNav = new System.Windows.Forms.FlowLayoutPanel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(24)))), ((int)(((byte)(30)))), ((int)(((byte)(54)))));
+            this.panel1.Controls.Add(this.panel3);
+            this.panel1.Controls.Add(this.btnBuscar);
+            this.panel1.Controls.Add(this.btnPrestamos);
+            this.panel1.Controls.Add(this.btnCerrar);
+            this.panel1.Controls.Add(this.btnCertificados);
+            this.panel1.Controls.Add(this.btnHipotecarios);
+            this.panel1.Controls.Add(this.btnAhorro);
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(186, 577);
+            this.panel1.TabIndex = 0;
+            // 
+            // panel3
+            // 
+            this.panel3.Location = new System.Drawing.Point(184, 563);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(766, 14);
+            this.panel3.TabIndex = 2;
+            // 
+            // btnBuscar
+            // 
+            this.btnBuscar.FlatAppearance.BorderSize = 0;
+            this.btnBuscar.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnBuscar.Font = new System.Drawing.Font("Arial", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnBuscar.ForeColor = System.Drawing.Color.White;
+            this.btnBuscar.Location = new System.Drawing.Point(3, 505);
+            this.btnBuscar.Name = "btnBuscar";
+            this.btnBuscar.Size = new System.Drawing.Size(186, 33);
+            this.btnBuscar.TabIndex = 6;
+            this.btnBuscar.Text = "Buscar cliente";
+            this.btnBuscar.UseVisualStyleBackColor = true;
+            // 
+            // btnPrestamos
+            // 
+            this.btnPrestamos.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnPrestamos.FlatAppearance.BorderSize = 0;
+            this.btnPrestamos.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnPrestamos.Font = new System.Drawing.Font("Arial", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnPrestamos.ForeColor = System.Drawing.Color.White;
+            this.btnPrestamos.Location = new System.Drawing.Point(0, 243);
+            this.btnPrestamos.Name = "btnPrestamos";
+            this.btnPrestamos.Size = new System.Drawing.Size(186, 33);
+            this.btnPrestamos.TabIndex = 5;
+            this.btnPrestamos.Text = "Prestamos personales";
+            this.btnPrestamos.UseVisualStyleBackColor = true;
+            // 
+            // btnCerrar
+            // 
+            this.btnCerrar.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.btnCerrar.FlatAppearance.BorderSize = 0;
+            this.btnCerrar.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnCerrar.Font = new System.Drawing.Font("Arial", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnCerrar.ForeColor = System.Drawing.Color.White;
+            this.btnCerrar.Location = new System.Drawing.Point(0, 544);
+            this.btnCerrar.Name = "btnCerrar";
+            this.btnCerrar.Size = new System.Drawing.Size(186, 33);
+            this.btnCerrar.TabIndex = 4;
+            this.btnCerrar.Text = "Cerrar seción";
+            this.btnCerrar.UseVisualStyleBackColor = true;
+            // 
+            // btnCertificados
+            // 
+            this.btnCertificados.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnCertificados.FlatAppearance.BorderSize = 0;
+            this.btnCertificados.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnCertificados.Font = new System.Drawing.Font("Arial", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnCertificados.ForeColor = System.Drawing.Color.White;
+            this.btnCertificados.Location = new System.Drawing.Point(0, 210);
+            this.btnCertificados.Name = "btnCertificados";
+            this.btnCertificados.Size = new System.Drawing.Size(186, 33);
+            this.btnCertificados.TabIndex = 3;
+            this.btnCertificados.Text = "certificados a plazo";
+            this.btnCertificados.UseVisualStyleBackColor = true;
+            // 
+            // btnHipotecarios
+            // 
+            this.btnHipotecarios.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnHipotecarios.FlatAppearance.BorderSize = 0;
+            this.btnHipotecarios.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnHipotecarios.Font = new System.Drawing.Font("Arial", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnHipotecarios.ForeColor = System.Drawing.Color.White;
+            this.btnHipotecarios.Location = new System.Drawing.Point(0, 177);
+            this.btnHipotecarios.Name = "btnHipotecarios";
+            this.btnHipotecarios.Size = new System.Drawing.Size(186, 33);
+            this.btnHipotecarios.TabIndex = 2;
+            this.btnHipotecarios.Text = "Prestamos hipotecarios";
+            this.btnHipotecarios.UseVisualStyleBackColor = true;
+            // 
+            // btnAhorro
+            // 
+            this.btnAhorro.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnAhorro.FlatAppearance.BorderSize = 0;
+            this.btnAhorro.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnAhorro.Font = new System.Drawing.Font("Arial", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnAhorro.ForeColor = System.Drawing.Color.White;
+            this.btnAhorro.Location = new System.Drawing.Point(0, 144);
+            this.btnAhorro.Name = "btnAhorro";
+            this.btnAhorro.Size = new System.Drawing.Size(186, 33);
+            this.btnAhorro.TabIndex = 1;
+            this.btnAhorro.Text = "Cuenta de ahorro";
+            this.btnAhorro.UseVisualStyleBackColor = true;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Controls.Add(this.pictureBox1);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(186, 144);
+            this.panel2.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial Black", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(53, 98);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(78, 23);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Usuario";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(61, 23);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(63, 63);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pnlNav
+            // 
+            this.pnlNav.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(126)))), ((int)(((byte)(249)))));
+            this.pnlNav.Location = new System.Drawing.Point(0, 150);
+            this.pnlNav.Name = "pnlNav";
+            this.pnlNav.Size = new System.Drawing.Size(3, 100);
+            this.pnlNav.TabIndex = 1;
+            // 
+            // panel4
+            // 
+            this.panel4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(24)))), ((int)(((byte)(30)))), ((int)(((byte)(54)))));
+            this.panel4.Location = new System.Drawing.Point(185, 563);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(766, 14);
+            this.panel4.TabIndex = 3;
+            // 
+            // panel5
+            // 
+            this.panel5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(24)))), ((int)(((byte)(30)))), ((int)(((byte)(54)))));
+            this.panel5.Location = new System.Drawing.Point(185, 0);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(766, 14);
+            this.panel5.TabIndex = 4;
+            // 
+            // dashboard
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(951, 577);
+            this.Controls.Add(this.panel5);
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.pnlNav);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "dashboard";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnBuscar;
+        private System.Windows.Forms.Button btnPrestamos;
+        private System.Windows.Forms.Button btnCerrar;
+        private System.Windows.Forms.Button btnCertificados;
+        private System.Windows.Forms.Button btnHipotecarios;
+        private System.Windows.Forms.Button btnAhorro;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.FlowLayoutPanel pnlNav;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Panel panel5;
+    }
+}
+
